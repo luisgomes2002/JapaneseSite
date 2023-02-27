f425d39d222fbd9237eecf94bfc25cc551a5ce2e
Luis <luisgmgomes@gmail.com>
2023-02-27 11:00:51 -0300

    server

---
+++ passport-config.js
@@ -0,0 +1,27 @@
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt = requeri('bcrypt')
+
+function initialize(passport, getUserByEmail) {
+    const authenticateUser = async (email, password, done) => {
+        const user = getUserByEmail(email)
+        if(user == null) {
+            return done(null, false, {message: 'No user with that email'})
+        }
+
+        try{
+            if(await bcrypt.compare(passport, user.password)){
+                return done(null, user)
+            }else{
+                return done(null, false, {mensage: 'Password incorrect'})
+            }
+        } catch(e){
+            return done(e)
+        }
+    }
+
+    passport.use(new LocalStrategy({ usernameFiel: 'email'}, authenticateUser))
+    passport.serializeUser((user, done) => { })
+    passport.desirializeUser((id, done) => { })
+}
+
+module.exports = initialize
---
+++ server.js
@@ -0,0 +1,66 @@
+if(process.env.NOD_ENV !== 'producrion') {
+    require('detenv').config()
+}
+
+//29:27min
+
+const express = require('express')
+const app = express()
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+
+const initializePassport = require('./passport-config')
+initializePassport(
+    passport, 
+    email => users.find(user => user.email === email)
+)
+
+const users = []
+
+app.set('view-engine', 'ejs')
+app.use(express.urlencoded({ extende: false }))
+app.use(flash())
+app.use(session({
+    secret: precess.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize)
+app.use(passport.session())
+
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+})
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
+})
+
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureFlash: '/login',
+    failureFlash: true
+}))
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs')
+})
+
+app.post('/register', async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        })
+        res.redirect('/login')
+    } catch { 
+        res.redirect('/register')
+    }
+})
+
+app.listen(5500)
