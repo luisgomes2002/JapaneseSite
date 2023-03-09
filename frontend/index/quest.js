e02681e969360fa263e22948a737ab9612decfba
Luis <luisgmgomes@gmail.com>
2023-03-09 01:12:36 -0300

    form

---
+++ frontend/index/quest.js
@@ -0,0 +1,110 @@
+var answers = {}
+
+var question_one = document.getElementById('quest__1')
+var question_two = document.getElementById('quest__2')
+var question_three = document.getElementById('quest__3')
+var question_four = document.getElementById('quest__4')
+var question_five = document.getElementById('quest__5')
+
+function storeAnswer(quest__number, event){
+    if(event.target.type === 'radio'){
+        console.log(event.target.value)
+        answers['question' + quest__number] = parseInt(event.target.value)
+        console.log(answers)
+    }
+}
+
+question_one.addEventListener('click', function(event){
+    storeAnswer(1, event)
+})
+
+question_two.addEventListener('click', function(event){
+    storeAnswer(2, event)
+})
+
+question_three.addEventListener('click', function(event){
+    storeAnswer(3, event)
+})
+
+question_four.addEventListener('click', function(event){
+    storeAnswer(4, event)
+})
+
+question_five.addEventListener('click', function(event){
+    storeAnswer(5, event)
+})
+
+function totalScore(){
+    var total_score = 
+    answers.question1 + 
+    answers.question2 + 
+    answers.question3 + 
+    answers.question4 + 
+    answers.question5 
+
+    return total_score
+}
+
+function getInfoBaseOnScore(){
+    if(totalScore() < 7){
+        var score_info = "7"
+    }else if(totalScore() >=7 && totalScore() <= 10) {
+        var score_info = ">7"
+    }else if(totalScore() > 10){
+        var score_info = "ok"
+    }
+
+    return score_info
+}
+
+var submit1 = document.getElementById('submit1')
+var submit2 = document.getElementById('submit2')
+var submit3 = document.getElementById('submit3')
+var submit4 = document.getElementById('submit4')
+var submit5 = document.getElementById('submit5')
+
+function nextQuestion(quest__number){
+    var current_quest_number = quest__number - 1
+    var quest__number = quest__number.toString()
+    var current_quest_number = current_quest_number.toString()
+
+    var el = document.getElementById('quest__'+ quest__number)
+    var el2 = document.getElementById('quest__'+current_quest_number)
+
+    el.style.display = "block"
+    el2.style.display = "none"
+}
+
+submit1.addEventListener('click', function(){
+    nextQuestion(2)
+    growProgressBar('40%')
+})
+
+submit2.addEventListener('click', function(){
+    nextQuestion(3)
+    growProgressBar('60%')
+})
+
+submit3.addEventListener('click', function(){
+    nextQuestion(4)
+    growProgressBar('70%')
+})
+
+submit4.addEventListener('click', function(){
+    nextQuestion(5)
+    growProgressBar('100%')
+})
+
+submit5.addEventListener('click', function(){
+    nextQuestion(6)
+})
+
+submit5.addEventListener('click',  function(){
+    document.getElementById('printtotalscore').innerHTML = totalScore()
+    document.getElementById('printsocreinfo').innerHTML = getInfoBaseOnScore()
+})
+
+function growProgressBar(percentage_width){
+    var bar = document.getElementById('progress__bar')
+    bar.style.width = percentage_width
+}
