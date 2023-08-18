755792ee148ba7a1fc1f752392b9933f6b1cb602
Luis <luisgmgomes@gmail.com>
2023-08-17 22:01:41 -0300

    feat: TOKEN

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -9,21 +9,20 @@ const login = async (req, res) => {
 
     if (!user) {
       return res.status(404).send({ message: 'User or Password not found' })
-    }
+    };
 
     const passwordIsValid = bcrypt.compareSync(password, user.password);
 
     if (!passwordIsValid) {
       return res.status(400).send({ message: 'User or Password not found' })
-    }
+    };
 
     const token = generateToken(user.id);
 
-    res.send(token)
+    res.send(token);
   } catch (err) {
     res.status(500).send(err.message);
-    console.log()
-  }
+  };
 };
 
 export { login };
