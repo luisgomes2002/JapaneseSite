dc1aeec9caf207026c90b35ca4d7160cb6e2edd7
Luis <luisgmgomes@gmail.com>
2024-01-16 22:14:59 -0300

    style: footer style

--- client/src/components/template/Footer.jsx
+++ client/src/components/template/Footer.jsx
@@ -1,60 +1,58 @@
-import React from "react";
-import "./Footer.css";
 import { Link } from "react-router-dom";
+import {
+  Credits,
+  FooterContainer,
+  SocialIcons,
+  SocialMediaWrap,
+} from "./FooterStyled";
 
-function Footer() {
+const Footer = () => {
   return (
     <footer>
-      <div className="footer-container">
+      <FooterContainer>
         <section>
-          <div className="social-icons">
+          <SocialIcons>
             <Link
               to="https://www.instagram.com/_gomesluis/"
-              className="social-icon--link"
               target="react/jsx-no-target-blank"
             >
               <i className="fa-brands fa-instagram"></i>
             </Link>
             <Link
               to="https://twitter.com/SAiForaMalucoXD"
-              className="social-icon--link"
               target="react/jsx-no-target-blank"
             >
               <i className="fa-brands fa-x-twitter"></i>
             </Link>
             <Link
               to="https://www.linkedin.com/in/luis-gomes-8462b321a/"
-              className="social-icon--link"
               target="react/jsx-no-target-blank"
             >
               <i className="fa-brands fa-linkedin"></i>
             </Link>
             <Link
               to="https://github.com/luisgomes2002"
-              className="social-icon--link"
               target="react/jsx-no-target-blank"
             >
               <i className="fa-brands fa-github"></i>
             </Link>
-          </div>
-          <div className="social-media--wrap">
-            <div className="footer-logo">
-              <Link to="/" id="footer-logo">
-                Murasaki
-              </Link>
+          </SocialIcons>
+          <SocialMediaWrap>
+            <div>
+              <Link to="/">Murasaki</Link>
             </div>
-            <p className="website-rights">© 2023. Murasaki</p>
-          </div>
-          <div className="creditos">
+            <p>© 2023. Murasaki</p>
+          </SocialMediaWrap>
+          <Credits>
             <p>
               <Link to="/creditsPage">Créditos das imagens ・</Link>
               <Link to="/portfolio"> Murasaki Products</Link>
             </p>
-          </div>
+          </Credits>
         </section>
-      </div>
+      </FooterContainer>
     </footer>
   );
-}
+};
 
 export default Footer;
